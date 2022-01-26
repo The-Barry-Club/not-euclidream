1e4b840b4feb277c2427b93ed73a82b44d80f809
Ilianus <formet.iliane@gmail.com>
2022-01-26 11:08:25 +0100

    Audio settings script (check si on veut la confirmation ou pas)

--- not-euclidream/Assets/Scripts/UI/Main menu/MenuController.cs
+++ not-euclidream/Assets/Scripts/UI/Main menu/MenuController.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuController : MonoBehaviour
 {
     [Header("Levels To Load")]
     public string _PlayGame;
-    
+
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+
+    [Header("Apply")]
+    [SerializeField] private GameObject confirmationPrompt = null;
+
     public void PlayBtn()
     {
         SceneManager.LoadScene(_PlayGame);
@@ -18,4 +26,24 @@ public class MenuController : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(Confirmation());
+    }
+
+    public IEnumerator Confirmation()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(1);
+        confirmationPrompt.SetActive(false);
+    }
+    
 }
