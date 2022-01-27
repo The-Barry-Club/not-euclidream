ddd93c5e84e8f3d8702b1ec6f297bb2de046cdf1
HugoS <hugo.schreiber@epita.fr>
2022-01-27 13:25:10 +0100

    2eme du git

---
+++ Assets/Scripts/Multiplayer/SpawnPlayers.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.PlayerLoop;
+using Random = System.Random;
+
+public class SpawnPlayers : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject ballPrefab;
+    public int minX;
+    public int maxX;
+    public int minZ;
+    public int maxZ;
+
+    private void Start()
+    {
+        int x = new Random().Next(minX, maxX);
+        int z = new Random().Next(minZ, maxZ);
+        Vector3 randomPos = new Vector3(x,1,z);
+        PhotonNetwork.Instantiate(playerPrefab.name, randomPos, Quaternion.identity);
+        PhotonNetwork.Instantiate(ballPrefab.name, ballPrefab.transform.position, Quaternion.identity);
+    }
+}
