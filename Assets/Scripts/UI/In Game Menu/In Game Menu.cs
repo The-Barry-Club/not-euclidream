4849d0665f6770a58f941306fd1aeb430d8357ea
Ilianus <formet.iliane@gmail.com>
2022-02-10 22:36:10 +0100

    ya rien qui marche ptdr

---
+++ Assets/Scripts/UI/In Game Menu/In Game Menu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class InGameMenu : MonoBehaviour
+{
+    public void InGameSettings()
+    if (Input.GetKeyDown(KeyCode.Escape))
+    {
+        
+    }
+}
