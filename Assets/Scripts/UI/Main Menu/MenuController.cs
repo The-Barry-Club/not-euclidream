dfe988135ac9a6a929bb57efb641829bc6a94e75
Ilianus <formet.iliane@gmail.com>
2022-04-26 13:52:24 +0200

    sound settings

--- Assets/Scripts/UI/Main Menu/MenuController.cs
+++ Assets/Scripts/UI/Main Menu/MenuController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuController : MonoBehaviour
 {
+    //MAIN MENU
     [Header("Levels To Load")]
     public string _PlayGame;
     
@@ -18,5 +20,29 @@ public class MenuController : MonoBehaviour
     public void Quit_YesBtn()
     {
         Application.Quit();
+    }
+    
+    
+    //VOLUME SETTINGS
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null; 
+    [SerializeField] private Slider volumeSlider = null;
+    
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        //StartCoroutine(Confirmation());
+    }
+    
+    public void VolumeBack()
+    {
+        float volume = PlayerPrefs.GetFloat("masterVolume");
+        volumeTextValue.text = volume.ToString("0.0");
+        volumeSlider.value = volume;
     }
 }
