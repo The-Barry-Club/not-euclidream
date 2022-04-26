d815d0f460aa3a62eb3a10d9e916796cf590522a
GnomeApprenti <42942956+GnomeApprenti@users.noreply.github.com>
2022-04-26 20:41:41 +0200

    Fixed portal camera orientation problem

--- Assets/Scripts/noteuclid/PortalCamera.cs
+++ Assets/Scripts/noteuclid/PortalCamera.cs
@@ -20,7 +20,7 @@ public class PortalCamera : MonoBehaviour
         Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
         transform.position = portal.position + playerOffsetFromPortal;
 
-        float angluarDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+        float angluarDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation)+180;
 
         Quaternion portalRotationalDifference = Quaternion.AngleAxis(angluarDifferenceBetweenPortalRotations, Vector3.up);
         Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
