9033411299d184d2c5eca2d71c87c8f5ff9c06c2
Hugo Schreiber <hugo.schreiber94@orange.fr>
2022-05-19 10:27:01 +0200

    Fucking genius

---
+++ Assets/Scripts/Player/Optimization.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Optimization : MonoBehaviour
+{
+
+
+    public Camera cam;
+    public Transform PlayerPos;
+
+    public List<GameObject> portalList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Portal"))
+        {
+            portalList.Add(obj);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        //Desactivate portals if they are too far
+        foreach (GameObject obj in portalList)
+        {
+            Debug.Log(obj.GetComponent<Transform>().position);
+            if(Vector3.Distance(PlayerPos.position,obj.GetComponent<Transform>().position) < 5)
+            {
+                obj.SetActive(true);
+                obj.GetComponent<CameraRef>().cam.SetActive(true);
+                Debug.Log("Visible");
+            }
+            else{
+                obj.SetActive(false);
+                obj.GetComponent<CameraRef>().cam.SetActive(false);
+                Debug.Log("not visible");
+            } 
+        }
+
+
+
+    }
+
+
+    private bool IsVisible(Camera c, GameObject target){
+
+        var planes = GeometryUtility.CalculateFrustumPlanes(c);
+        var point = target.transform.position;
+
+        foreach (var plane in planes)
+        {
+            if(plane.GetDistanceToPoint(point) > 0)
+                return false;
+        
+        }
+        return true;
+    }
+
+}
