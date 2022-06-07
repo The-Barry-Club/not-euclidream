35992bbba8bfb1f10cea8eb1e2b26a4aa0dc6158
Aleksei <yacar0009@gmail.com>
2022-06-07 14:07:33 +0300

    collisions overhaul v2

--- Assets/Scripts/4D/PlayerRayMarchCollider.cs
+++ Assets/Scripts/4D/PlayerRayMarchCollider.cs
@@ -14,7 +14,7 @@ namespace Unity.Mathematics
 
         public float colliderOffset = 1.2f;
         public float maxDownMovement = 1f;
-        [Tooltip ("The transforms from which the raymarcher will test the distances and apply the collision (6 colliders from 4dCplliders)")]
+        [Tooltip("The transforms from which the raymarcher will test the distances and apply the collision (6 colliders from 4dCplliders)")]
         public Transform[] rayMarchTransforms;
 
         private DistanceFunctions Df;
@@ -32,30 +32,35 @@ namespace Unity.Mathematics
         // Start is called before the first frame update
         void Start()
         {
-            camScript = _camera.GetComponent<RaymarchCam>();
             Df = GetComponent<DistanceFunctions>();
             if (obj.tag == "Player")
             {
                 pm = GetComponent<PlayerMovement>();
                 controller = GetComponent<CharacterController>();
+                camScript = _camera.GetComponent<RaymarchCam>();
                 isPlayer = true;
             }
-                
+            else
+            {
+                _camera = Camera.main;
+                camScript = _camera.GetComponent<RaymarchCam>();
+            }
+
         }
-        
+
         // Update is called once per frame
         void Update()
         {
             if (!isPlayer)
                 MoveToGround();
             RayMarch(rayMarchTransforms);
-            
+
         }
         // the distancefunction for the shapes
         public float GetShapeDistance(Shape4D shape, float4 p4D)
         {
-            p4D -= (float4) shape.Position();
-           
+            p4D -= (float4)shape.Position();
+
             p4D.xz = mul(p4D.xz, float2x2(cos(shape.Rotation().y), sin(shape.Rotation().y), -sin(shape.Rotation().y), cos(shape.Rotation().y)));
             p4D.yz = mul(p4D.yz, float2x2(cos(shape.Rotation().x), -sin(shape.Rotation().x), sin(shape.Rotation().x), cos(shape.Rotation().x)));
             p4D.xy = mul(p4D.xy, float2x2(cos(shape.Rotation().z), -sin(shape.Rotation().z), sin(shape.Rotation().z), cos(shape.Rotation().z)));
@@ -75,7 +80,7 @@ namespace Unity.Mathematics
                     return Df.sdHypersphere(p4D, shape.Scale().x);
 
                 case Shape4D.ShapeType.DuoCylinder:
-                    return Df.sdDuoCylinder(p4D, ((float4) shape.Scale()).xy);
+                    return Df.sdDuoCylinder(p4D, ((float4)shape.Scale()).xy);
                 case Shape4D.ShapeType.plane:
                     return Df.sdPlane(p4D, shape.Scale());
                 case Shape4D.ShapeType.Cone:
@@ -170,7 +175,7 @@ namespace Unity.Mathematics
         void MoveToGround()
         {
             Vector3 p = transform.position;
-           //check hit
+            //check hit
 
             float d = DistanceField(p);
             d = Mathf.Min(d, maxDownMovement);
