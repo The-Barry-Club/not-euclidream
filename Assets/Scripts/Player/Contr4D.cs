05cfcfc6e4365f55ef64d2c5886c3b4c8b7590ee
Aleksei <yacar0009@gmail.com>
2022-06-08 23:25:02 +0300

    plus que scenery

--- Assets/Scripts/Player/Contr4D.cs
+++ Assets/Scripts/Player/Contr4D.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Mathematics;
 
 public class Contr4D : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class Contr4D : MonoBehaviour
     public List<Shape4D> sh4d;
 
     public Quest q;
+    public bool last;
 
     public bool goodrot = false;
     bool allowrot = true;
@@ -16,6 +18,14 @@ public class Contr4D : MonoBehaviour
     public bool goodposx = false;
     bool allowposx = true;
     public float stopposx;
+
+    public bool goodposz = false;
+    bool allowposz = true;
+    public float stopposz;
+
+    public bool goodposy = false;
+    bool allowposy = true;
+    public float stopposy;
     // Start is called before the first frame update
     void Start()
     {
@@ -44,9 +54,10 @@ public class Contr4D : MonoBehaviour
                     foreach (Shape4D s in sh4d)
                     {
                         s.rotationW.y += 1f;
-                        if (goodrot && allowrot && (s.rotationW.y > stoprot || s.rotationW.y < -stoprot))
+                        if (goodrot && allowrot && (s.rotationW.y == stoprot || s.rotationW.y == -stoprot))
                         {
-                            q.current++;
+                            if (!last)
+                                q.current++;
                             allowrot = false;
                         }
                     }
@@ -55,15 +66,16 @@ public class Contr4D : MonoBehaviour
                     foreach (Shape4D s in sh4d)
                     {
                         s.rotationW.y -= 1f;
-                        if (goodrot && allowrot && (s.rotationW.y > stoprot || s.rotationW.y < -stoprot))
+                        if (goodrot && allowrot && (s.rotationW.y == stoprot || s.rotationW.y == -stoprot))
                         {
-                            q.current++;
+                            if (!last)
+                                q.current++;
                             allowrot = false;
                         }
                     }
-            if (Input.GetKey("k"))
-                if (allowposx)
-                    foreach (var s in shapes)
+            if (Input.GetKey("k") && allowposx && goodposx)
+            {
+                foreach (var s in shapes)
                     {
                         s.transform.position += new Vector3(0.1f, 0, 0);
                         if (goodposx && allowposx && (s.transform.position.x < stopposx + 0.3f && s.transform.position.x > stopposx - 0.3f))
@@ -72,10 +84,11 @@ public class Contr4D : MonoBehaviour
                             allowposx = false;
                         }
                     }
+            }
 
-            if (Input.GetKey("j"))
-                if (allowposx)
-                    foreach (var s in shapes)
+            if (Input.GetKey("j") && allowposx && goodposx)
+            {
+                foreach (var s in shapes)
                     {
                         s.transform.position += new Vector3(-0.1f, 0, 0);
                         if (goodposx && allowposx && (s.transform.position.x < stopposx + 0.3f && s.transform.position.x > stopposx - 0.3f))
@@ -84,6 +97,55 @@ public class Contr4D : MonoBehaviour
                             allowposx = false;
                         }
                     }
+            }
+
+            if (Input.GetKey("g") && allowposz && goodposz)
+            {
+                foreach (var s in shapes)
+                    {
+                        s.transform.position += new Vector3(0, 0, 0.1f);
+                        if (goodposz && allowposz && (s.transform.position.z < stopposz + 0.3f && s.transform.position.z > stopposz - 0.3f))
+                        {
+                            allowposz = false;
+                        }
+                    }
+            }
+
+            if (Input.GetKey("h") && allowposz && goodposz)
+            {
+                foreach (var s in shapes)
+                    {
+                        s.transform.position += new Vector3(0, 0, -0.1f);
+                        if (goodposz && allowposz && (s.transform.position.z < stopposz + 0.3f && s.transform.position.z > stopposz - 0.3f))
+                        {
+                            allowposz = false;
+                        }
+                    }
+            }
+
+            if (Input.GetKey("o") && allowposy && goodposy)
+            {
+                foreach (var s in shapes)
+                    {
+                        s.transform.position += new Vector3(0, 0.1f, 0);
+                        if (goodposy && allowposy && (s.transform.position.y < stopposy + 0.3f && s.transform.position.y > stopposy - 0.3f))
+                        {
+                            allowposy = false;
+                        }
+                    }
+            }
+
+            if (Input.GetKey("p") && allowposy && goodposy)
+            {
+                foreach (var s in shapes)
+                    {
+                        s.transform.position += new Vector3(0, -0.1f, 0);
+                        if (goodposy && allowposy && (s.transform.position.y < stopposy + 0.3f && s.transform.position.y > stopposy - 0.3f))
+                        {
+                            allowposy = false;
+                        }
+                    }
+            }
         }
     }
 }
