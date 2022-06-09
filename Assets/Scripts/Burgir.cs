9f594298ad7eee6fbf35f4531bd05fc0151dc283
Aleksei <yacar0009@gmail.com>
2022-06-09 23:54:17 +0300

    ff

--- Assets/Scripts/Burgir.cs
+++ Assets/Scripts/Burgir.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class Burgir : MonoBehaviour
 {
     public GameObject burgir;
+    public AudioSource au;
     // Start is called before the first frame update
     void Start()
     {
-        
+        au = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (transform.position.y < 3.2f)
+        {
+            au.Play();
             burgir.SetActive(true);
+        }
         else
             burgir.SetActive(false);
     }
