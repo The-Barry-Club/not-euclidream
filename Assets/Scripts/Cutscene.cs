c5c48272a0c46a574f5c04d0fb90895d9b73eab9
Aleksei <yacar0009@gmail.com>
2022-06-09 20:18:49 +0300

    cat talks

---
+++ Assets/Scripts/Cutscene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cutscene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Wpostrig.cs
+++ Assets/Scripts/Wpostrig.cs
@@ -6,6 +6,9 @@ public class Wpostrig : MonoBehaviour
 {
     public Camera cam;
     public RaymarchCam rcam;
+    public GameObject cutcam;
+    public GameObject timeline;
+    public GameObject cat;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +20,25 @@ public class Wpostrig : MonoBehaviour
     {
         if (other.tag == "Player")
         {
+            this.gameObject.GetComponent<BoxCollider>().enabled = false;
             rcam._wPosition = 0;
             rcam.wpos.SetActive(false);
             rcam.waxe = false;
+            cat.GetComponent<CatController>().enabled = false;
+            cat.transform.position = new Vector3(-224.81893920898438f,1.6593523025512696f,-496.6573791503906f);
+            cat.transform.rotation = new Quaternion(0, 0.922334552f, 0, 0.386392355f);
+            cutcam.SetActive(true);
+            timeline.SetActive(true);
+            StartCoroutine(Fincut());
+
         }
+    }
+
+    IEnumerator Fincut()
+    {
+        yield return new WaitForSeconds(4);
+        cutcam.SetActive(false);
+        cat.GetComponent<CatController>().enabled = true;
+        timeline.SetActive(false);
     }
 }
